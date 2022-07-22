6a6669ea13c2e5d7bdb5a402c5812592be6d3676
yejunho10 <hyjcompany30@naver.com>
2022-07-22 15:17:31 +0900

    dev #3

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const bodyParser = require("body-parser")
 const express = require("express")
 const {Client} = require("pg");
-const nodemailer = require("nodemailer")
+const crypto = require("crypto")
+const fs = require("fs")
+// const nodemailer = require("nodemailer")
 
 const app = express()
 const port = app.listen(5050)
@@ -18,12 +20,32 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + "/web"))
 client.connect()
 
-app.post("/postTest", function(req, res) {
+app.post("/postTest", function(req) {
     console.log(req.body.p1)
 })
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/web/main/index.html")
+})
+
+app.get("/downloadUUID", function(req, res) {
+    var uuid = crypto.randomUUID()
+    var filename = uuid + ".uid"
+    var filepath = __dirname + "/tmp/" + filename
+    var stream = fs.createWriteStream(filepath)
+
+    stream.once('open', function() {
+        stream.write(uuid)
+        stream.end()
+
+        res.download(filepath, "UUID.uid", function(err) {
+            if (err) console.log(err)
+
+            fs.unlinkSync(filepath)
+            console.log("successfully deleted tmp file..")
+        })
+        console.log("successfully sent file to client..")
+    })
 })
 
 app.listen(port, function() {
