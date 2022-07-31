48a62eabea936c1e433572a481a2f09d39d399cc
yejunho10 <hyjcompany30@naver.com>
2022-07-31 15:50:36 +0900

    dev #17

--- routes/store.js
+++ routes/store.js
@@ -6,9 +6,9 @@ router
     .get("/", (req, res) => {
         const context = {
             products: [
-                { product_id: "test", title: "test", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test", price: "1" }, // using forEach
-                { product_id: "test", title: "test", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test", price: "2" },
-                { product_id: "test", title: "test", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test", price: "3" }
+                { product_id: "test1", title: "test1", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test1", price: "1" }, // using forEach
+                { product_id: "test2", title: "test2", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test2", price: "2" },
+                { product_id: "test3", title: "test3", thumbnail_path: "../../public/assets/img/logo-transparent.png", tag: "test3", price: "3" }
             ],
             product_count: 3,
         }
@@ -23,6 +23,7 @@ router
         })
     })
     .get("/detail", (req, res) => {
+        const id = req.query.id
         const context = {
             product_title: "",
             tag: "",
@@ -34,6 +35,8 @@ router
             img_4_path: "",
             img_5_path: ""
         }
+
+        res.end(id + " -> detail page!")
 
         res.render("store/product_detail.ejs", context, (err, html) => {
             if (err) {
