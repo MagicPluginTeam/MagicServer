f79e721dd37ea778ee163d4c1e25fe3e99455585
yejunho10 <hyjcompany30@naver.com>
2022-10-08 14:28:53 +0900

    dev #63

--- schema/RedirectSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const redirectSchema  = new mongoose.Schema({
-    directCode: { type: String, required: true, unique: true },
-    url: { type: String, required: true, unique: false }
-})
-
-module.exports = redirectSchema
