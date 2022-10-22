adff997d433d0c570d663329a6b2df9f89d08542
yejunho10 <hyjcompany30@naver.com>
2022-10-22 10:18:56 +0900

    feat: remove api docs

--- src/routes/api/v1/product.js
+++ src/routes/api/v1/product.js
@@ -4,39 +4,6 @@ const db = require("../../../modules/database");
 let router = express.Router();
 
 router
-    /**
-     * @api {get} /api/v1/:queryType/:query/:dataType Request product information.
-     *
-     * @apiVersion 1.0.0
-     * @apiName getProductInformation
-     * @apiGroup product
-     *
-     * @apiParam {String} queryType QueryType. ["id", "title"]
-     * @apiParam {String} query Query string.
-     * @apiParam {String} dataType DataType. ["all", "productId", "title", "shortDescription", "description", "tag", "price", "createAt", "lastUpdateAt", "thumbnailImageURL", "productImageURL", "buys"]
-     *
-     * @apiSuccessExample {json} SuccessExample
-     *     HTTP/1.1 200 OK
-     *     {
-     *         "status": "DONE",
-     *         "msg": "SUCCESS",
-     *         "data": "29604aef-c6fc-4669-b069-903a555f05b9"
-     *     }
-     * @apiSuccess {String} status Response status. ["DONE", "ERROR"]
-     * @apiSuccess {String} msg Response status msg. ["SUCCESS", "INVALID_QUERY_TYPE", "INVALID_QUERY", "INVALID_DATA_TYPE"]
-     * @apiSuccess {Any} data Requested data.
-     *
-     * @apiErrorExample {json} ErrorExample
-     *     HTTP/1.1 403 ERROR
-     *     {
-     *         "status": "ERROR",
-     *         "msg": "NO_PERMISSION",
-     *         "data": null
-     *     }
-     * @apiError {String} status Response status. ["DONE", "ERROR"]
-     * @apiError {String} msg Response status msg. ["SUCCESS", "INVALID_QUERY_TYPE", "INVALID_QUERY", "INVALID_DATA_TYPE"]
-     * @apiError {Any} data Always null.
-     */
     .get("/:queryType/:query/:dataType", async (req, res) => {
         let product;
         let queryType = req.params.queryType;
